d3c22f7d8a7c17cdf717e5bba769299fac5256e3
nokoran <58602078+nokoran@users.noreply.github.com>
2022-10-08 21:10:31 +0300

    added README

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 
-Console.WriteLine("Hello, World!");
+namespace lab1
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
