a66aa4c6f839372748270d60f517dc1615a7fd66
nokoran <58602078+nokoran@users.noreply.github.com>
2022-10-08 22:55:41 +0300

    second

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -1,12 +1,21 @@
-﻿
-
-namespace lab1
+﻿namespace lab1
 {
     static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter n (max number of elements in subset):");
+            int n = Int32.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+            Console.WriteLine("Enter k (number of elements overall):");
+            int k = Int32.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+            double total = 0;
+            while (n > 0)
+            {
+                total += Math.Pow(k, n);
+                n--;
+            }
+
+            Console.WriteLine($"Total is {total}");
         }
     }
 }
