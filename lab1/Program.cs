3c487352605f90d7e464e34e5500fade0bd28634
nokoran <58602078+nokoran@users.noreply.github.com>
2022-10-19 20:37:23 +0300

    lab1 - done

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -4,19 +4,51 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter n (max number of elements in subset):");
-            int n = Int32.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
-            Console.WriteLine("Enter k (number of elements overall):");
-            int k = Int32.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
-            double total = 0;
-            while (n > 0)
+            var a = Int32.Parse(Console.ReadLine()!);
+            var b = Int32.Parse(Console.ReadLine()!);
+            var c = Int32.Parse(Console.ReadLine()!);
+            
+            List<int> a_combinations = new List<int>();
+            List<int> b_combinations = new List<int>();
+            List<(int x, int y)> result = new List<(int x, int y)>();
+            
+            a_combinations.Add(a);
+            b_combinations.Add(b);
+            
+            for (int i = 1; i < a.ToString().Length; i++)
             {
-                total += Math.Pow(k, n);
-                n--;
+                a_combinations.Add(Int32.Parse(a.ToString().Substring(i) + a.ToString().Substring(0, i)));
+            }
+            
+            for (int i = 1; i < b.ToString().Length; i++)
+            {
+                b_combinations.Add(Int32.Parse(b.ToString().Substring(i) + b.ToString().Substring(0, i)));
             }
 
-            Console.WriteLine($"Total is {total}");
+            foreach (var ai in a_combinations)
+            {
+                foreach (var bi in b_combinations)
+                {
+                    if (ai + bi == c)
+                    {
+                        result.Add((ai, bi));
+                    }
+                }
+            }
+
+            Console.WriteLine("Result:");
+
+            if (!result.Any())
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                Console.WriteLine("YES");
+                var temp = result.FirstOrDefault(o => o.x == result.Min(r => r.x));
+                Console.WriteLine(temp.x);
+                Console.WriteLine(temp.y);
+            }
         }
     }
 }
-
